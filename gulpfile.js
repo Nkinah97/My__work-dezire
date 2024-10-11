3f59f30972a366c055ef5caac63f118d1a0dd6cb
Nkinah97 <nkinah93@gmail.com>
2024-10-11 19:19:07 +0300

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
 const del = require('del');
-const cache = require('gulp-cache'); // Додано для очищення кешу
+const cache = require('gulp-cache');
 
 let autoprefixer, imagemin;
 
@@ -16,7 +16,7 @@ async function loadModules() {
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: './'
     },
     scrollProportionally: true,
     scrollRestoreTechnique: 'cookie',
@@ -29,7 +29,7 @@ function cleanDist() {
 
 async function images() {
   await loadModules();
-  return src('app/images/**/*')
+  return src('images/**/*')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -49,41 +49,41 @@ async function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/mixitup/dist/mixitup.js',
-    'app/js/main.js', 
+    'js/main.js', 
   ], { allowEmpty: true })
     .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(dest('app/js'))
+    .pipe(dest('js'))
     .pipe(browserSync.stream());
 }
 
 async function styles() {
   await loadModules();
-  return src('app/scss/style.scss')
+  return src('scss/style.scss')
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
+      overrideBrowserslist: ['last 10 versions'],
       grid: true
     }))
-    .pipe(dest('app/css'))
+    .pipe(dest('css'))
     .pipe(browserSync.stream());
 }
 
 function build() {
   return src([
-    'app/css/style.min.css',
-    'app/fonts/**/*',
-    'app/js/main.min.js',
-    'app/*.html'
-  ], { base: 'app' })
+    'css/style.min.css',
+    'fonts/**/*',
+    'js/main.min.js',
+    '*.html'
+  ], { base: '.' })
     .pipe(dest('dist'));
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/*.html']).on('change', browserSync.reload);
+  watch(['scss/**/*.scss'], styles);
+  watch(['js/**/*.js', '!js/main.min.js'], scripts);
+  watch(['*.html']).on('change', browserSync.reload);
 }
 
 function clearCache(done) {
